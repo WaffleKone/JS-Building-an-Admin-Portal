b18c2b05c079b1b37fc85d2593ed479bcc9858d4
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-04-23 13:04:04 -0400

    added admin panel

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
+// fetch('http://localhost:3001/updateBook',
+//   {
+//     method: 'PATCH',
+//     body: JSON.stringify({
+//       id: 3,
+//       title: 'The Legends of Arathrae',
+//     }),
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//   }).then((response) => response.json());
 
-// Your Code Here
+// the above code updates the 3rd book selection to The Legends of Arathrae
+
+async function grabBooks() {
+  let response = await fetch('http://localhost:3001/listBooks');
+  let books = await response.json();
+  books.forEach(displayBook);
+}
+
+function displayBook(book) {
+  let root = document.querySelector('#root');
+  let li = document.createElement('li');
+  li.textContent = book.title;
+
+  let quantityBox = document.createElement('input');
+  quantityBox.value = book.quantity;
+
+  let confirm = document.createElement('button');
+  confirm.textContent = 'Confirm';
+
+  confirm.addEventListener('click', () => {
+    fetch('http://localhost:3001/updateBook', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: book.id,
+        quantity: quantityBox.value,
+      }),
+    });
+  });
+  li.append(quantityBox, confirm);
+  root.append(li);
+}
+
+grabBooks();
